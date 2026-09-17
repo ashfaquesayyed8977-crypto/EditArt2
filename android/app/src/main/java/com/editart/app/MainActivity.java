3a74ba4e625745655b3ace8716aeb881b97fcc5b
ashfaquesayyed8977-crypto <ashfaquesayyed8977@gmail.com>
2026-09-17 11:55:53 +0000

    add android folder and capacitor config

---
+++ android/app/src/main/java/com/editart/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.editart.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.editart.app',
+  appName: 'EditArt',
+  webDir: 'dist'
+};
+
+export default config;
